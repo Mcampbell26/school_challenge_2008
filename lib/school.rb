fa1bf69e59cc90f6e24ed6cb08ec96fe57c21a65
Megan Campbell <megan.campbell26@yahoo.com>
2020-08-19 13:41:22 -0600

    Add iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,19 @@
 class School
-  attr_reader :start_time, :hours_in_school_day
+  attr_reader :start_time, :hours_in_school_day, :student_names, :end_time
 
-  def initialize(start, hours)
+  def initialize(start, hour)
     @start_time = start
     @hours_in_school_day = hour
+    @student_names = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    # start_time.to_i + (hours_in_school_day * 100)
+    # end_time.to_s
+    # require 'pry'; binding pry
   end
 end
